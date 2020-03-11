3b410e51825b79778525ff49a0cf67d35310a0c8
sellabae <devsellabae@gmail.com>
2020-03-10 21:11:45 -0700

    created a maven project

---
+++ ontologygenerator/src/main/java/Main.java
@@ -0,0 +1,14 @@
+import org.apache.jena.propertytable.graph.GraphCSV;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+
+//        Model model = ModelFactory.createModelForGraph(new GraphCSV("data.csv")) ;
+
+    }
+
+}
