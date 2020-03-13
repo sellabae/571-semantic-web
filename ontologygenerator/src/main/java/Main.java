9aaaeabc95ca3b9bd76cf6af7ae4c9316d7c6251
Yashua <ovando.yashua@gmail.com>
2020-03-13 16:54:18 -0700

    implemented basic structure for rdf

--- ontologygenerator/src/main/java/Main.java
+++ ontologygenerator/src/main/java/Main.java
@@ -1,14 +1,33 @@
-import org.apache.jena.propertytable.graph.GraphCSV;
-import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.*;
+import org.apache.jena.vocabulary.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
 
+        //structure of the csv file ROW = SUBJECT   COLUMN NAME = CLASSES (for now )  CELL = object (literal or resource)
+        Model model = basicStructure();
+      
+        //Writing RDF
+        model.write(System.out);
 
-//        Model model = ModelFactory.createModelForGraph(new GraphCSV("data.csv")) ;
+    } // end of main
 
+    public static Model basicStructure() {
+        // Create an empty model for the CSV File
+        Model model = ModelFactory.createDefaultModel();
+
+        // Create an empty resource
+        Resource test = model.createResource();
+
+        // add property to relate subject and object where "Predicate is the object (in
+        // this case a literal")
+        test.addProperty(VCARD.N, "Left Side of Tree");
+
+        // to create another empty resource that points to another resource
+        test.addProperty(VCARD.N,
+                model.createResource().addProperty(VCARD.N, "left branch").addProperty(VCARD.N, "right branch"));
+
+        return model;
     }
 
 }
