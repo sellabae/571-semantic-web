9b961d496b0c4de591790d35640c6588a7acec26
matthewnguyen <matthewnguyen19@gmail.com>
2020-03-16 14:10:41 -0700

    Meteorite.java updates

--- ontologygenerator/src/main/java/Meteorites.java
+++ ontologygenerator/src/main/java/Meteorites.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.nio.file.Paths;
 
 
+import com.fasterxml.jackson.databind.annotation.JsonAppend;
 import org.apache.jena.rdf.model.*;
 import org.apache.jena.vocabulary.*;
 
@@ -27,13 +28,12 @@ public class Meteorites {
         }
     }
 
-    // build solar model
+    // build meteorite model
     public static Model convertCsv2rdf() throws IOException {
         // opening the csv file
 
         String absolutePath = Paths.get(".").toAbsolutePath().normalize().toString();
         String pathToSolar = absolutePath + "\\ontologygenerator\\dataset\\meteorites.csv";
-        // System.out.println(lunarFile.getAbsolutePath());
 
         BufferedReader fileReader = new BufferedReader(new FileReader(pathToSolar));
         String csv_row = fileReader.readLine();
@@ -60,7 +60,6 @@ public class Meteorites {
         // }
 
         return model;
-        // System.out.println("Lunar File Exists...Now Reading its content");
     }
 
     public static Model MeteoriteBaseModel( String[] column_names, Model model, String[] csv_row_cells) {
@@ -68,14 +67,18 @@ public class Meteorites {
         // String owlNamespace= "http://www.w3.org/2002/07/owl#";
         // String xsdNamespace = "http://www.w3.org/2001/XMLSchema#";
         // String exNamespace = "http://example.org/time/";
+        String georssNamespace = "http://www.georss.org/georss";
+
+        Resource meteoriteResource = model.createResource("https://data.nasa.gov/Space-Science/Meteorite-Landings/gh4g-9sfh/data");
 
         /*------------------------------------------- [Name] -----------------------------------------*/
 
         //TO-DO: This should actually be in <Description rdf:about=name>
 
         Resource meteorite = model.createResource();
-        Literal literalName = model.createLiteral(csv_row_cells[0]);
-        meteorite.addLiteral(RDF.subject, literalName);
+        Property hasName = model.createProperty("http://webprotege.stanford.edu/R8IqsOPT1nR9XAQSrFuh2vl", "hasName");
+//        Literal literalName = model.createLiteral(csv_row_cells[0]);
+        meteorite.addLiteral(hasName, csv_row_cells[0]);
         /*------------------------------------------- [Year] -----------------------------------------*/
 
         Resource year = model.createResource();
@@ -89,43 +92,69 @@ public class Meteorites {
 
         /*---------------------------------------------[Mass]-----------------------------------------*/
         Resource mass = model.createResource();
-        Literal literalMass = model.createLiteral(csv_row_cells[2]);
-        mass.addLiteral(RDF.subject, literalMass);
+        Property weighs = model.createProperty("http://webprotege.stanford.edu/R7WItf8cFKHXAgXpcn9qAII", "weighs");
+//        Literal literalMass = model.createLiteral(csv_row_cells[2]);
+        mass.addLiteral(weighs, csv_row_cells[2]);
 
         /*-------------------------------------------[Fell or Found] ----------------------------------------*/
         Resource fellFound = model.createResource();
-        Literal literalFell = model.createLiteral(csv_row_cells[3]);
-        fellFound.addLiteral(RDF.subject, literalFell);
+        Property wasFoundOrFell = model.createProperty("http://webprotege.stanford.edu/RN4zvRqrViPXwGVRK0RC4F", "wasFoundOrFell");
+//        Literal literalFell = model.createLiteral(csv_row_cells[3]);
+        fellFound.addLiteral(wasFoundOrFell, csv_row_cells[3]);
 
         /*--------------------------------------------[Geolocation ] ----------------------------------------*/
 
+        // find uri for the geolocation , latitude, and longitude in georss ontology
+        Property geoPoint = model.createProperty(georssNamespace, "point");
+        Property geoLat = model.createProperty(georssNamespace, "lat");
+        Property geoLong = model.createProperty(georssNamespace, "long");
+
         // create geolocation node and make the instance of
-        Resource geoLocation = model.createResource();
+        Resource geoLocation = model.createResource(geoPoint);
 
         // create latitude node and points it to the latitude literal value
-        Resource latitude = model.createResource(); // creates the node for the latitude
-        Literal latValue = model.createLiteral(csv_row_cells[5]); // prepares the literal value that the node will                                                           // point to
-        latitude.addLiteral(RDF.subject, latValue); // assigns the literal value of the latitude
-        latitude.addProperty(RDF.type, geoLocation); // makes the latitude of type Geolocation
+        Resource latitude = model.createResource(geoLat);
+        Literal latiValue = model.createLiteral(csv_row_cells[5]);
+        Property isLatitude = model.createProperty("http://webprotege.stanford.edu/RBgyEpVqD0AV1ILL37Mm3QF",
+                "isLatitude");
+        latitude.addLiteral(isLatitude, latiValue);
 
         // create longitude node and points it to the longitude literal value
-        Resource longitude = model.createResource();
-        Literal longValue = model.createLiteral(csv_row_cells[6]);
-        longitude.addLiteral(RDF.subject, longValue);
-        longitude.addProperty(RDF.type, geoLocation);
+        Resource longitude = model.createResource(geoLong);
+        Literal longiValue = model.createLiteral(csv_row_cells[6]);
+        Property isLongitude = model.createProperty("http://webprotege.stanford.edu/RCuDcxjZyI5mrZERCOYhR6V",
+                "isLongitude");
+        longitude.addLiteral(isLongitude, longiValue);
 
-        // creates the statement for the geolocation has latitude and longitude
-        model.add(geoLocation, RDF.predicate, latitude);
-        model.add(geoLocation, RDF.predicate, longitude);
+        /*--------------------------------------------[Model Statements] ---------------------------------------*/
+
+        // adding thetogether Long and Lat as point and forms a statement
+        Property hasLatitude = model.createProperty("http://webprotege.stanford.edu/R71035Ho9VoqMTn7bzIc21B",
+                "hasLatitude");
+        Property hasLongitude = model.createProperty("http://webprotege.stanford.edu/R9FbDFOr8bMgMHTcqF2Gxij",
+                "hasLongitude");
+        Property hasPoint = model.createProperty("http://www.opengis.net/gml", "Point");
+
+        model.add(geoLocation, hasLongitude, longitude);
+        model.add(geoLocation, hasLatitude, latitude);
+        model.add(geoLocation, hasPoint, csv_row_cells[5] + " " + csv_row_cells[6]);
+
 
         /*--------------------------------------------[Model Statements] ----------------------------------------*/
 
-        model.add(meteorite, RDF.predicate, year);
-        model.add(meteorite, RDF.predicate, recClass);
-        model.add(meteorite, RDF.predicate, mass);
-        model.add(meteorite, RDF.predicate, fellFound);
-        model.add(meteorite, RDF.predicate, geoLocation);
+        Property hasYear = model.createProperty("http://webprotege.stanford.edu/RDomYoSiLe5SCG2vckwHgFi", "hasYear");
+        Property hasMass = model.createProperty("http://webprotege.stanford.edu/R839CxlOT9WSMg6HLCXhXuA", "hasMass");
+        Property fellOrFound = model.createProperty("http://webprotege.stanford.edu/R9TqYzBYag5SoPJCuwz3TzX", "hasFellOrFound");
+        Property hasGeolocation = model.createProperty("http://webprotege.stanford.edu/R7zoJhYOcFDOQk8Gn6eHIxC",
+                "hasGeolocation");
 
+        model.add(meteorite, hasYear, year);
+        model.add(meteorite, RDF.predicate, recClass);
+        model.add(meteorite, hasMass, mass);
+        model.add(meteorite, fellOrFound, fellFound);
+        model.add(meteorite, hasGeolocation, geoLocation);
+
+        meteorite.addProperty(RDF.type, meteoriteResource);
         return model;
     }
 }
