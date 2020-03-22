73eb6eadad6d5e853ffe73c8d36afef344ec66f8
Yashua <ovando.yashua@gmail.com>
2020-03-22 14:17:52 -0700

    commented out lines in main

--- ontologygenerator/src/main/java/Main.java
+++ ontologygenerator/src/main/java/Main.java
@@ -9,21 +9,14 @@ public class Main {
 
         Model solarModel = SolarEclipse.convertCsv2rdf();
         SolarEclipse.writeRDFOutputFile(solarModel);
-        SolarEclipse.validateSolarModel(solarModel);
+        //SolarEclipse.validateSolarModel(solarModel);
         Model meteoriteModel = Meteorites.convertCsv2rdf();
         Meteorites.writeRDFOutputFile(meteoriteModel);
 
         Model ufoModel = UFOSightings.convertCsv2rdf();
         UFOSightings.writeRDFOutputFile(ufoModel);
-        UFOSightings.validateUFOModel(ufoModel);
+        //UFOSightings.validateUFOModel(ufoModel);
         
-        /*
-         * TO-DO: 
-         *  1) CREATE OTHER MODELS
-         *  2) IMPLEMENT OOD FOR CLEANER STRUCTURE 
-         * 3) MAKE SURE OUR FORM FOR THE NODES ARE CORRECT THE
-         * PROPERTY TYPES AND SO ON
-         */
 
     } // end of main
 
