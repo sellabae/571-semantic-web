49611d5c3d2f9740ef1ed9f2b7e719c2f677a8fb
Yashua <ovando.yashua@gmail.com>
2020-03-26 15:52:28 -0700

    created sparql query branch , class for queries, outline of possible code

---
+++ ontologygenerator/src/main/java/SparqlQueries.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.nio.file.Paths;
+import java.util.Iterator;
+
+import org.apache.jena.rdf.model.*;
+import org.apache.jena.reasoner.ValidityReport;
+import org.apache.jena.vocabulary.*;
+
+public class SparqlQueries {
+
+    public static void queryModel(File file) {
+
+
+        //Open rdf file containing models
+
+
+
+        //create an empty in-memory model and populate it from our graph
+
+
+
+        //create a new query
+
+
+        //form queries in string format
+
+
+        //create a new query from the QueryFactory
+
+
+        //Exectue teh query and obtain results
+
+
+
+        //output query results
+
+
+        //free up resources used in running the query
+
+    }
+
+}
